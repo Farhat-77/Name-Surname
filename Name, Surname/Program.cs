8afd4844aa28768f53e7960ef49d3e2dc0fa4e36
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2020-12-21 22:46:58 +0600

    Добавьте файлы проекта.

---
+++ Name, Surname/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Name__Surname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Farhat Bolatuly!");
+            Console.ReadKey(true);
+        }
+    }
+}
